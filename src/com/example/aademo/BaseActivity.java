66dec6dcba84625b54a0f876da4ad1959f54bde1
徐 高翔 <mik.eddy186@gmail.com>
2014-05-29 10:32:19 +0800

    new develop new2

--- src/com/example/aademo/BaseActivity.java
+++ src/com/example/aademo/BaseActivity.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 @SuppressLint("ShowToast")
 public class BaseActivity extends Activity {
-	//new
+	//new1
 	Toast mToast=null;
 	public static final int LONGTOASTDURATION = 2500;
 	public Context mContext;
