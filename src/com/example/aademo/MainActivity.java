01f95082ca9a06309f071350506c1b28b74c41ce
xugaoxiang <xugaoxiang@ucfgroup.com>
2014-12-24 12:01:59 +0800

    不需要的内容删除

--- src/com/example/aademo/MainActivity.java
+++ src/com/example/aademo/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends BaseActivity {
 		linParent=(LinearLayout)findViewById(R.id.main_parentlin);
 		listItemHolders.add(new MainItemHolder("滑动动画开关", SlideParent_Activity.class));
 		listItemHolders.add(new MainItemHolder("自定义seekbar", CustomSeekBarActivity.class));
-		listItemHolders.add(new MainItemHolder("横向带纹理进度条", HorizontalProgressBarActivity.class));
+		listItemHolders.add(new MainItemHolder("横向进度条", HorizontalProgressBarActivity.class));
 		listItemHolders.add(new MainItemHolder("层图片", LayersDrawableActivity.class));
 		listItemHolders.add(new MainItemHolder("滑动遮盖效果", CoverActivity.class));
 		listItemHolders.add(new MainItemHolder("圆形图片", CircleImageActivity.class));
@@ -41,10 +41,8 @@ public class MainActivity extends BaseActivity {
 		listItemHolders.add(new MainItemHolder("横向滑动的listview", HorizontalListViewActivity.class));
 		listItemHolders.add(new MainItemHolder("翻阅listview源码后根据自己的理解写出的一些小玩意", CustomViewGroupActivity.class));
 		listItemHolders.add(new MainItemHolder("图片的手动控制旋转", WheelMenuActivity.class));
-		listItemHolders.add(new MainItemHolder("控件旋转", RotateActivity.class));
 		listItemHolders.add(new MainItemHolder("仿新版QQ侧滑", ResideMenuActivity.class));
 		listItemHolders.add(new MainItemHolder("雪花旋转进度条", SnowProgressActivity.class));
-		listItemHolders.add(new MainItemHolder("ListView的保持不动的item", StickyActivity.class));
 		listItemHolders.add(new MainItemHolder("跳转到其他app", GotoOtherAPPActivity.class));
         listItemHolders.add(new MainItemHolder("带闪光灯的二维码扫描", CaptureActivity.class));
         listItemHolders.add(new MainItemHolder("Event Bus 全局通信DEMO", EventBusActivity.class));
