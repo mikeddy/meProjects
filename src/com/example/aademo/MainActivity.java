259942b9e061cf428915a7359d9f52f22030cc9e
xugaoxiang <xugaoxiang@ucfgroup.com>
2015-01-28 15:27:10 +0800

    结构调整

--- src/com/example/aademo/MainActivity.java
+++ src/com/example/aademo/MainActivity.java
@@ -2,14 +2,12 @@ package com.example.aademo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-
-import com.xgx.testlibs.utils.Util;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -19,8 +17,8 @@ import zxing.activity.CaptureActivity;
 
 public class MainActivity extends BaseActivity {
 	
-	ArrayList<MainItemHolder>listItemHolders=new ArrayList<MainActivity.MainItemHolder>();
-	LinearLayout linParent;
+    ListView lv;
+    ArrayList<ItemBean>arraylist_itemBeans=new ArrayList<ItemBean>();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,52 +27,85 @@ public class MainActivity extends BaseActivity {
 	}
 	
 	private void init(){
-		linParent=(LinearLayout)findViewById(R.id.main_parentlin);
-		listItemHolders.add(new MainItemHolder("滑动动画开关", SlideParent_Activity.class));
-		listItemHolders.add(new MainItemHolder("自定义seekbar", CustomSeekBarActivity.class));
-		listItemHolders.add(new MainItemHolder("横向进度条", HorizontalProgressBarActivity.class));
-		listItemHolders.add(new MainItemHolder("层图片", LayersDrawableActivity.class));
-		listItemHolders.add(new MainItemHolder("滑动遮盖效果", CoverActivity.class));
-		listItemHolders.add(new MainItemHolder("圆形图片", CircleImageActivity.class));
-		listItemHolders.add(new MainItemHolder("下拉放大效果", TouchScaleActivity.class));
-		listItemHolders.add(new MainItemHolder("Text颜色渐变AND圆形进度条", CircleProgressActivity.class));
-		listItemHolders.add(new MainItemHolder("自定义drawable.xml的深入研究", DrawableXMLActivity.class));
-		listItemHolders.add(new MainItemHolder("横向滑动的listview", HorizontalListViewActivity.class));
-		listItemHolders.add(new MainItemHolder("翻阅listview源码后根据自己的理解写出的一些小玩意", CustomViewGroupActivity.class));
-		listItemHolders.add(new MainItemHolder("图片的手动控制旋转", WheelMenuActivity.class));
-		listItemHolders.add(new MainItemHolder("仿新版QQ侧滑", ResideMenuActivity.class));
-		listItemHolders.add(new MainItemHolder("雪花旋转进度条", SnowProgressActivity.class));
-        listItemHolders.add(new MainItemHolder("带闪光灯的二维码扫描", CaptureActivity.class));
-        listItemHolders.add(new MainItemHolder("Event Bus 全局通信DEMO", EventBusActivity.class));
-        listItemHolders.add(new MainItemHolder("EffectPath 逐步动画实现", EffectPathActivity.class));
+        lv=(ListView)findViewById(R.id.main_lv);
+		arraylist_itemBeans.add(new ItemBean("滑动动画开关", SlideParent_Activity.class));
+		arraylist_itemBeans.add(new ItemBean("自定义seekbar", CustomSeekBarActivity.class));
+		arraylist_itemBeans.add(new ItemBean("横向进度条", HorizontalProgressBarActivity.class));
+		arraylist_itemBeans.add(new ItemBean("层图片", LayersDrawableActivity.class));
+		arraylist_itemBeans.add(new ItemBean("滑动遮盖效果", CoverActivity.class));
+		arraylist_itemBeans.add(new ItemBean("圆形图片", CircleImageActivity.class));
+		arraylist_itemBeans.add(new ItemBean("下拉放大效果", TouchScaleActivity.class));
+		arraylist_itemBeans.add(new ItemBean("Text颜色渐变AND圆形进度条", CircleProgressActivity.class));
+		arraylist_itemBeans.add(new ItemBean("自定义drawable.xml的深入研究", DrawableXMLActivity.class));
+		arraylist_itemBeans.add(new ItemBean("横向滑动的listview", HorizontalListViewActivity.class));
+		arraylist_itemBeans.add(new ItemBean("翻阅listview源码后根据自己的理解写出的一些小玩意", CustomViewGroupActivity.class));
+		arraylist_itemBeans.add(new ItemBean("图片的手动控制旋转", WheelMenuActivity.class));
+		arraylist_itemBeans.add(new ItemBean("仿新版QQ侧滑", ResideMenuActivity.class));
+		arraylist_itemBeans.add(new ItemBean("雪花旋转进度条", SnowProgressActivity.class));
+        arraylist_itemBeans.add(new ItemBean("带闪光灯的二维码扫描", CaptureActivity.class));
+        arraylist_itemBeans.add(new ItemBean("Event Bus 全局通信DEMO", EventBusActivity.class));
+        arraylist_itemBeans.add(new ItemBean("EffectPath 逐步动画实现", EffectPathActivity.class));
+
+        MainAdapter adapter=new MainAdapter();
+        lv.setAdapter(adapter);
+	}
 
 
-        Util util=new Util();
-        util.MyUtil();
-		for (int i = 0; i < listItemHolders.size(); i++) {
-			linParent.addView(listItemHolders.get(i).btn);
-		}
-	}
-	
-	
-	class MainItemHolder{
-		public MainItemHolder(String title,Class<?>cla){
-			c=cla;
-			if(btn==null){
-				btn=new Button(mContext);
-				btn.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.MATCH_PARENT));
-				btn.setGravity(Gravity.CENTER);
-				btn.setText(title);
-				btn.setOnClickListener(new OnClickListener() {
-					@Override
-					public void onClick(View v) {
-						Intent intent=new Intent(mContext, c);
-						startActivity(intent);
-					}
-				});
-			}
-		}
-		public Button btn;
-		Class<?> c;
-	}
+    class MainAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return arraylist_itemBeans.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Button btn;
+            final ItemBean itembean=arraylist_itemBeans.get(position);
+            if(convertView==null){
+                convertView=getLayoutInflater().inflate(R.layout.main_item,null);
+                btn=(Button)convertView.findViewById(R.id.main_item_btn);
+                convertView.setTag(btn);
+            }else{
+                btn=(Button)convertView.getTag();
+            }
+            btn.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent=new Intent(mContext, itembean.getCla());
+                    startActivity(intent);
+                }
+            });
+            return convertView;
+        }
+    }
+
+
+    class ItemBean{
+        private String name;
+        private Class<?>cla;
+        public ItemBean(String n,Class<?> c){
+         name=n;
+         cla=c;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Class<?> getCla() {
+            return cla;
+        }
+
+    }
 }
