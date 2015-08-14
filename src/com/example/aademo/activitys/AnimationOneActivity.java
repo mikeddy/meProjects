578b0ff4c3d7d12c118712af07f60808c668a248
mik.eddy <mik.eddy186@gmail.com>
2015-08-14 22:43:45 +0800

    细节调整

--- src/com/example/aademo/activitys/AnimationOneActivity.java
+++ src/com/example/aademo/activitys/AnimationOneActivity.java
@@ -1,16 +1,26 @@
 package com.example.aademo.activitys;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.aademo.R;
+import com.example.aademo.impl.ViewAdapter;
+import com.example.aademo.widget.CustomViewGroup;
 
 /**
  * Created by mik_eddy on 15/8/14.
  */
 public class AnimationOneActivity extends BaseActivity {
     TextView tv_plan,tv_planbottom;
+    CustomViewGroup cvg_bg;
+    public static final int STARTCOUNT=20;//一共有多少颗星星
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +33,53 @@ public class AnimationOneActivity extends BaseActivity {
     private void init(){
         tv_plan=(TextView)findViewById(R.id.animtion1_plan);
         tv_planbottom=(TextView)findViewById(R.id.animtion1_planbottom);
+        cvg_bg=(CustomViewGroup)findViewById(R.id.animtion1_cvg_bg);
     }
 
     private void processLogic() {
-        tv_plan.startAnimation(AnimationUtils.loadAnimation(this,R.anim.anim_animation_1_plan));
-        tv_planbottom.startAnimation(AnimationUtils.loadAnimation(this,R.anim.anim_animation_1_planbottom));
+        tv_plan.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_animation_1_plan));
+        tv_planbottom.startAnimation(AnimationUtils.loadAnimation(this, R.anim.anim_animation_1_planbottom));
+        mHandler.sendEmptyMessageDelayed(0, 1000);
+
     }
 
+    Handler mHandler=new Handler(){
+        @Override
+        public void dispatchMessage(Message msg) {
+            super.dispatchMessage(msg);
+            loadBgStartAnimation();
+        }
+    };
+
+
+    private void loadBgStartAnimation(){
+        cvg_bg.setAdapter(new ViewAdapter() {
+
+            @Override
+            public int getViewCount() {
+                return 1;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                ImageView img;
+                if (convertView == null) {
+                    convertView = LayoutInflater.from(mContext).inflate(R.layout.item_animation_star, null);
+                    img = ((ImageView) convertView.findViewById(R.id.item_animation_img_star));
+                    convertView.setTag(img);
+                } else {
+                    img = (ImageView) convertView.getTag();
+                }
+                return convertView;
+            }
+        });
+//        cvg_bg.setDir(CustomViewGroup.DIRECTION.BOTTOM2TOP);
+        cvg_bg.startAnimation();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+//        cvg_bg.stop();
+    }
 }
--- src/com/example/aademo/widget/CustomViewGroup.java
+++ src/com/example/aademo/widget/CustomViewGroup.java
@@ -1,1 +1,141 @@
-package com.example.aademo.widget;import java.util.LinkedList;import java.util.Queue;import android.content.Context;import android.os.Handler;import android.util.AttributeSet;import android.view.View;import android.view.ViewGroup;import android.widget.LinearLayout;import com.example.aademo.R;import com.example.aademo.bean.MoveBean;import com.example.aademo.impl.ViewAdapter;public class CustomViewGroup extends ViewGroup {	ViewAdapter mAdapter;	int TopCount = 0;// 已经弹幕出来的个数	private Queue<View> mRemovedViewQueue = new LinkedList<View>();	public static final int MAXCOUNT = 10;	public int defaultMaxCount = MAXCOUNT;	boolean isStart = false;	public CustomViewGroup(Context context) {		super(context);	}	public CustomViewGroup(Context context, AttributeSet attrs) {		super(context, attrs);	}	public CustomViewGroup(Context context, AttributeSet attrs, int defStyle) {		super(context, attrs, defStyle);	}	@Override	protected void onLayout(boolean changed, int l, int t, int r, int b) {		reNewPosition();		removeNonVisibleItems();	}	public void startAnimation() {		if (isStart) return;		isStart = true;		Thread t = new Thread(new Runnable() {			@Override			public void run() {				while (true) {					mHandler.sendEmptyMessage(0);					try {						Thread.sleep(10);					} catch (Exception e) {					}				}			}		});		t.start();	}	Handler mHandler = new Handler() {		public void handleMessage(android.os.Message msg) {			requestLayout();		};	};	private void addChildView(View v) {		LayoutParams params = v.getLayoutParams();		if (params == null) {			params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);		}		addViewInLayout(v, -1, params, true);		v.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.AT_MOST));	}	private void reNewPosition() {		if (mAdapter == null)			return;		int childCount = getChildCount();		for (int i = 0; i < childCount; i++) {			View childView = getChildAt(i);			MoveBean bean = getMoveBeanFromTag(childView);			if (bean != null) {				int t = reLayout(childView, bean);				bean.setPointY(t - bean.getSpeed());				childView.setTag(R.id.tag_moveitem, bean);			}		}		if (childCount < defaultMaxCount) {			int viewCount = mAdapter.getViewCount();			if (TopCount >= viewCount)				TopCount = 0;			defaultMaxCount = viewCount < MAXCOUNT ? viewCount : MAXCOUNT;			View v = mAdapter.getView(TopCount, mRemovedViewQueue.poll(), this);			addChildView(v);			MoveBean moveBean = new MoveBean(v, getWidth(), getHeight());			v.setTag(R.id.tag_moveitem, moveBean);			reLayout(v, moveBean);			TopCount++;		}	}	private int reLayout(View childView, MoveBean bean) {		int l = bean.getPotinX();		int t = bean.getPointY();		int r = bean.getPotinX() + childView.getMeasuredWidth();		int b = bean.getPointY() + childView.getMeasuredHeight();		childView.layout(l, t, r, b);		return t;	}	private void removeNonVisibleItems() {		if (mAdapter == null)			return;		int startIndex = getChildCount() - 1;		while (startIndex > 0) {			View child = getChildAt(startIndex);			MoveBean moveBean = getMoveBeanFromTag(child);			if (moveBean != null) {				if (child.getBottom() - moveBean.getSpeed() <= 0) {					removeViewInLayout(child);					mRemovedViewQueue.offer(child);				}			}			startIndex--;		}	}	private MoveBean getMoveBeanFromTag(View v) {		Object object = v.getTag(R.id.tag_moveitem);		MoveBean bean = null;		if (object != null)			bean = (MoveBean) object;		return bean;	}	public void setAdapter(ViewAdapter adapter) {		mAdapter = adapter;	}}
+package com.example.aademo.widget;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aademo.R;
+import com.example.aademo.bean.MoveBean;
+import com.example.aademo.impl.ViewAdapter;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CustomViewGroup extends ViewGroup {
+	ViewAdapter mAdapter;
+	int TopCount = 0;// 已经弹幕出来的个数
+	private Queue<View> mRemovedViewQueue = new LinkedList<View>();
+	public static final int MAXCOUNT = 10;
+	public int defaultMaxCount = MAXCOUNT;
+	boolean isStart = false;
+
+	public CustomViewGroup(Context context) {
+		super(context);
+	}
+
+	public CustomViewGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CustomViewGroup(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		reNewPosition();
+		removeNonVisibleItems();
+	}
+
+	public void startAnimation() {
+		if (isStart) return;
+		isStart = true;
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					mHandler.sendEmptyMessage(0);
+					try {
+						Thread.sleep(10);
+					} catch (Exception e) {
+					}
+				}
+			}
+		});
+
+		t.start();
+	}
+
+	Handler mHandler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			requestLayout();
+		};
+	};
+
+	private void addChildView(View v) {
+		LayoutParams params = v.getLayoutParams();
+		if (params == null) {
+			params = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+		}
+		addViewInLayout(v, -1, params, true);
+		v.measure(MeasureSpec.makeMeasureSpec(getWidth(), MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(getHeight(), MeasureSpec.AT_MOST));
+	}
+
+	private void reNewPosition() {
+		if (mAdapter == null)
+			return;
+		int childCount = getChildCount();
+		for (int i = 0; i < childCount; i++) {
+			View childView = getChildAt(i);
+			MoveBean bean = getMoveBeanFromTag(childView);
+			if (bean != null) {
+				int t = reLayout(childView, bean);
+				bean.setPointY(t - bean.getSpeed());
+				childView.setTag(R.id.tag_moveitem, bean);
+			}
+		}
+		if (childCount < defaultMaxCount) {
+			int viewCount = mAdapter.getViewCount();
+			if (TopCount >= viewCount)
+				TopCount = 0;
+			defaultMaxCount = viewCount < MAXCOUNT ? viewCount : MAXCOUNT;
+			View v = mAdapter.getView(TopCount, mRemovedViewQueue.poll(), this);
+			addChildView(v);
+			MoveBean moveBean = new MoveBean(v, getWidth(), getHeight());
+			v.setTag(R.id.tag_moveitem, moveBean);
+			reLayout(v, moveBean);
+			TopCount++;
+		}
+	}
+
+	private int reLayout(View childView, MoveBean bean) {
+		int l = bean.getPotinX();
+		int t = bean.getPointY();
+		int r = bean.getPotinX() + childView.getMeasuredWidth();
+		int b = bean.getPointY() + childView.getMeasuredHeight();
+		childView.layout(l, t, r, b);
+		return t;
+	}
+
+	private void removeNonVisibleItems() {
+		if (mAdapter == null)
+			return;
+		int startIndex = getChildCount() - 1;
+		while (startIndex > 0) {
+			View child = getChildAt(startIndex);
+			MoveBean moveBean = getMoveBeanFromTag(child);
+			if (moveBean != null) {
+				if (child.getBottom() - moveBean.getSpeed() <= 0) {
+					removeViewInLayout(child);
+					mRemovedViewQueue.offer(child);
+				}
+			}
+			startIndex--;
+		}
+	}
+
+	private MoveBean getMoveBeanFromTag(View v) {
+		Object object = v.getTag(R.id.tag_moveitem);
+		MoveBean bean = null;
+		if (object != null)
+			bean = (MoveBean) object;
+		return bean;
+	}
+
+	public void setAdapter(ViewAdapter adapter) {
+		mAdapter = adapter;
+	}
+
+}
