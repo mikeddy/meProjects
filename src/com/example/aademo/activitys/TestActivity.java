9adf46b5f9ef42dfad42af63a9aafc647ce8fc8d
mik.eddy <mik.eddy186@gmail.com>
2015-08-27 22:36:58 +0800

    细节修改

---
+++ src/com/example/aademo/activitys/TestActivity.java
@@ -0,0 +1,23 @@
+package com.example.aademo.activitys;
+
+import android.os.Bundle;
+
+import com.example.aademo.R;
+
+/**
+ * Created by mik_eddy on 15/8/14.
+ */
+public class TestActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        init();
+    }
+
+    
+
+
+    private void init(){
+    }
+}
