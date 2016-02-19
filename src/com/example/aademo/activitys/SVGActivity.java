ede852e6ebf96949a5dfac4501d79b8a8ec9d0e0
mik.eddy <mik.eddy186@gmail.com>
2016-02-19 17:11:32 +0800

    细节修改

--- src/com/example/aademo/activitys/SVGActivity.java
+++ src/com/example/aademo/activitys/SVGActivity.java
@@ -1,6 +1,5 @@
 package com.example.aademo.activitys;
 
-import android.graphics.Path;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
@@ -12,37 +11,20 @@ import com.example.aademo.widget.PathView;
  * Created by mik_eddy on 15/12/25.
  */
 public class SVGActivity extends BaseActivity {
-    PathView mPathView;
     PathView mPathSvg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_svg);
-        mPathView = (PathView) findViewById(R.id.pathView);
         mPathSvg = (PathView) findViewById(R.id.pathSvg);
 
-
-        setMyPath();
-
-        mPathSvg.useNaturalColors();
+//        mPathSvg.setFill(true);
         mPathSvg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mPathSvg.getPathAnimator().delay(100).duration(1500).interpolator(new AccelerateDecelerateInterpolator()).start();
             }
         });
-    }
-
-    public void setMyPath() {
-        final Path path = new Path();
-        path.moveTo(100,100);
-        path.lineTo(100, 400);
-        path.close();
-
-
-        mPathView.setPath(path);
-
-
     }
 }
