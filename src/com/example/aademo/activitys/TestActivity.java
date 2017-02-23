92ec96ee986001c375fbbe8a52a25267fc35eb59
mik.eddy <mik.eddy186@gmail.com>
2017-02-23 17:51:38 +0800

    增加测试类

--- src/com/example/aademo/activitys/TestActivity.java
+++ src/com/example/aademo/activitys/TestActivity.java
@@ -2,13 +2,35 @@ package com.example.aademo.activitys;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.aademo.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by mik_eddy on 15/8/14.
  */
 public class TestActivity extends Activity {
+    @Bind(R.id.textlin)
+    LinearLayout lin_parent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        ButterKnife.bind(this);
+        init();
+    }
+
+    private void init(){
+        View v1= View.inflate(this, R.layout.item_test, null);
+        lin_parent.addView(v1);
+
+//          LayoutInflater.from(this).inflate(R.layout.item_test, lin_parent, false);
+
+//          LayoutInflater.from(this).inflate(R.layout.item_test, lin_parent, true);
     }
 }
