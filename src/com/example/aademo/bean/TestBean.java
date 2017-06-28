0431338a90143ae4e93059101fe9e31400ae9e32
mik.eddy <mik.eddy186@gmail.com>
2017-06-28 21:35:48 +0800

    rx提交

---
+++ src/com/example/aademo/bean/TestBean.java
@@ -0,0 +1,26 @@
+package com.example.aademo.bean;
+
+/**
+ * Created by mik_eddy on 2017/6/28.
+ */
+
+public class TestBean {
+    String age;
+    String name;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
