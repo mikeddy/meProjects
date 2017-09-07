c74b23060fba7ac95c1980e12d3507cab1c99eb6
mik.eddy <mik.eddy186@gmail.com>
2017-09-07 10:43:10 +0800

    提交文字库

--- src/com/example/aademo/activitys/TestActivity.java
+++ src/com/example/aademo/activitys/TestActivity.java
@@ -3,10 +3,8 @@ package com.example.aademo.activitys;
 import android.os.Bundle;
 
 import com.example.aademo.R;
-import com.example.aademo.widget.MarqueeTextView;
 
 public class TestActivity extends BaseActivity {
-     MarqueeTextView marqueeTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +13,5 @@ public class TestActivity extends BaseActivity {
 
     private void init(){
         setContentView(R.layout.activity_test);
-        marqueeTextView=(MarqueeTextView)findViewById(R.id.marqueetext);
-        marqueeTextView.setText("aaaabbbbccc");
     }
 }
